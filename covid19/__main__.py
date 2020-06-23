Minor fixes, data visualization and Correlation Matrix computation added

covid19/__main__.py
@@ -1,3 +1,4 @@
+import numpy as np
 from joblib import load
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
@@ -19,59 +20,76 @@ def main():
     # Load the data set
     X, y = dl.read_data(fl=(DP['CATFL']+ DP['CONFL']), ll=DP['LL'])
 
-    # # Stratified Splitting of the dataset
-    # X_tr, X_t, y_tr, y_t = dl.stratified_split(X, y, DP['SR'], DP['SEED'])
+    X_fs = X
+    if (args.dataAnalytics):
+        # instantiate data analytics
+        da = DataAnalytics(dl.data, X, y)
 
-    # instantiate data analytics
-    da = DataAnalytics(X, y)
+        print(f'Number of data points in class 1: {da.p_y()}')
+        print('\n---------------**********---------------\n')
 
-    X_fs, fs = da.select_features(DP['CATFL'], DP['CONFL'],\
-                                    k_cat=5, k_con=3,\
-                                    cat_mode='chi2',\
-                                    con_mode='anova_f')
-    print(fs)
-    print('\n---------------**********---------------\n')
+        print(f'Auto Correlation Matrix: {da.correlation_matrix()}')
+        print('\n---------------**********---------------\n')
+
+
+        print(f'SNR for numerical variables:\n')
+        print(da.s2nr(confl=DP['CONFL']))
+        print('\n---------------**********---------------\n')
+
+        X_fs, fs = da.select_features(DP['CATFL'], DP['CONFL'],\
+                                        k_cat=DP['NUM_CAT'],\
+                                        k_con=DP['NUM_CON'],\
+                                        cat_mode='chi2',\
+                                        con_mode='anova_f')
+        print(f'Selected Categorical Features:\n')
+        print(np.array(DP['CATFL'])[fs[0]])
+
+        print(f'\nSelected Numerical Features:')
+        print(np.array(DP['CONFL'])[fs[1]])
+        print('\n---------------**********---------------\n')
+
+        da.visualize(np.array(DP['CATFL'])[fs[0]], FP['CAT_GRAPH'])
+        da.visualize(np.array(DP['CONFL'])[fs[1]], FP['CON_GRAPH'])
+
+    # Stratified Splitting of the dataset
     X_tr, X_t, y_tr, y_t = dl.stratified_split(X_fs, y, DP['SR'], DP['SEED'])
 
-    # # Obtain analytics
-    # print(da.p_y())
-
     if (args.trainSVM):
-        SVM(X_tr, y_tr, 'linear', './saved_models/SVM/linear.joblib')
-        SVM(X_tr, y_tr, 'rbf', './saved_models/SVM/rbf.joblib')
+        SVM(X_tr, y_tr, 'rbf', FP['SVM_RBF'])
+        SVM(X_tr, y_tr, 'linear', FP['SVM_LINEAR'])
+
+        # Load Saved SVM models and evaluate model
+        rbf_svm_mdl = load(FP['SVM_RBF'])
+        y_p = rbf_svm_mdl.predict(X_t)
+        print(f'RBF Kernel SVM Accuracy: {accuracy_score(y_p, y_t)}')
+        print(f'RBF Kernel SVM Precision: {precision_score(y_p, y_t)}')
+        print(f'RBF Kernel SVM Recall: {recall_score(y_p, y_t)}')
+        print(f'RBF Kernel SVM F1: {f1_score(y_p, y_t)}')
+        print('\n---------------**********---------------\n')
+
+        linear_svm_mdl = load(FP['SVM_LINEAR'])
+        y_p = linear_svm_mdl.predict(X_t)
+        print(f'Linear Kernel SVM Accuracy: {accuracy_score(y_p, y_t)}')
+        print(f'Linear Kernel SVM Precision: {precision_score(y_p, y_t)}')
+        print(f'Linear Kernel SVM Recall: {recall_score(y_p, y_t)}')
+        print(f'Linear Kernel SVM F1 Score: {f1_score(y_p, y_t)}')
+        print('\n---------------**********---------------\n')
     
     if (args.trainRF):
-        random_forest(X_tr, y_tr, './saved_models/RF/rf.joblib')
+        random_forest(X_tr, y_tr, FP['RFMDL'])
+        
+        # Load Random Forest Classifier
+        rfc = load(FP['RFMDL'])
+        y_p = rfc.predict(X_t)
+        print(f'RFC Accuracy: {accuracy_score(y_p, y_t)}')
+        print(f'RFC Precision: {precision_score(y_p, y_t)}')
+        print(f'RFC Recall: {recall_score(y_p, y_t)}')
+        print(f'RFC F1: {f1_score(y_p, y_t)}')
+        print('\n---------------**********---------------\n')
 
     if (args.trainNN):
         train_ffnn(X_tr, y_tr, './saved_models/NN/FFNN1.joblib')
         test_ffnn(X_t, y_t, './saved_models/NN/FFNN1.joblib')
-
-    # Load Saved SVM models and measure accuracies
-    rbf_svm_mdl = load('./saved_models/SVM/rbf.joblib')
-    y_p = rbf_svm_mdl.predict(X_t)
-    print(f'RBF Kernel SVM Accuracy: {accuracy_score(y_p, y_t)}')
-    print(f'RBF Kernel SVM Precision: {precision_score(y_p, y_t)}')
-    print(f'RBF Kernel SVM Recall: {recall_score(y_p, y_t)}')
-    print(f'RBF Kernel SVM F1: {f1_score(y_p, y_t)}')
-    print('\n---------------**********---------------\n')
-
-    linear_svm_mdl = load('./saved_models/SVM/linear.joblib')
-    y_p = linear_svm_mdl.predict(X_t)
-    print(f'Linear Kernel SVM Accuracy: {accuracy_score(y_p, y_t)}')
-    print(f'Linear Kernel SVM Precision: {precision_score(y_p, y_t)}')
-    print(f'Linear Kernel SVM Recall: {recall_score(y_p, y_t)}')
-    print(f'Linear Kernel SVM F1 Score: {f1_score(y_p, y_t)}')
-    print('\n---------------**********---------------\n')
-
-    # Load Random Forest Classifier
-    rfc = load('./saved_models/RF/rf.joblib')
-    y_p = rfc.predict(X_t)
-    print(f'RFC Accuracy: {accuracy_score(y_p, y_t)}')
-    print(f'RFC Precision: {precision_score(y_p, y_t)}')
-    print(f'RFC Recall: {recall_score(y_p, y_t)}')
-    print(f'RFC F1: {f1_score(y_p, y_t)}')
-    print('\n---------------**********---------------\n')
 
     return 0
 

covid19/data/data_analytics.py
@@ -1,13 +1,16 @@
 import numpy as np
 import pandas as pd
+import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.feature_selection import SelectKBest, chi2, mutual_info_classif, f_classif
 
+from ..utilities.config import DP, FP
 class DataAnalytics:
-    def __init__(self, X, y):
+    def __init__(self, data, X, y):
         super().__init__()
         self.X = X
         self.y = y
+        self.data = data
     
     def p_y(self):
         """ Input - 
@@ -18,8 +21,21 @@ class DataAnalytics:
         """
         return self.y.sum()/self.y.count()
 
-    def correlation(self):
+    def s2nr(self, confl):
+        X0 = self.X.loc[(self.y.to_numpy() == 0), confl]
+        X1 = self.X.loc[(self.y.to_numpy() == 1), confl]
+        return (abs(X0.mean() - X1.mean())).div((X0.std() + X1.std()))
+
+    def visualize(self, ll, fn):
+        plt.figure()
+        sns.pairplot(self.data[ll])
+        plt.savefig(fn)
         return None
+
+    def correlation_matrix(self):
+        cor = self.data.corr()
+        cor.to_csv(FP['CORR'])
+        return cor
     
     def select_features(self, catfl, confl, k_cat=None, k_con=None, cat_mode=None, con_mode=None): 
         """ Input -
@@ -68,10 +84,21 @@ class DataAnalytics:
                     fs.get_support(indices=True)
         
         def anova_f_selection(self, confl=confl, k=k_con):
+            """ Input -
+                    confl: (list) continuous label features headers list
+                    k: (int) number of features desired. default: All features
+                Output -
+                    - displays the graph of negative log p_values and prints the p_values too
+                    - X transformed with the best k categorical features based on 
+                    - The array of index of features selected based on one way ANOVA using
+                        the F statistic (consequently the p_values)
+            """
             fs = SelectKBest(score_func=f_classif, k=k)
             fs.fit(self.X[confl], np.ravel(self.y))
             scores = -np.log10(fs.pvalues_)
             scores /= scores.max()
+            print(f'p_values: {fs.pvalues_}')
+            print('\n---------------**********---------------\n')
             plt.figure()
             plt.bar(np.arange(self.X[confl].shape[1]) - .45, scores, width=.2,\
             label=r'Univariate score ($-Log(p_{value})$)')
@@ -80,7 +107,7 @@ class DataAnalytics:
             plt.yticks(())
             plt.axis('tight')
             plt.legend(loc='upper right')
-            # plt.show()
+            plt.savefig(FP['RFIMG'])
             return fs.transform(self.X[confl]), fs.get_support(indices=True)
         
         # Select Categorical Features
@@ -93,6 +120,6 @@ class DataAnalytics:
         if con_mode is 'anova_f':
             X_con, con_fs = anova_f_selection(self, confl, k_con)
 
-        print(X_cat.shape, X_con.shape)
+        # print(X_cat.shape, X_con.shape)
         return np.concatenate((X_cat, X_con), axis=1),\
                 [cat_fs, con_fs]

covid19/ml/ffnn.py
@@ -1,11 +1,9 @@
 import tensorflow as tf
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-from numpy.random import seed
 from ..utilities.config import DP
 
-seed(DP['SEED'])
-tf.set_random_seed(DP['SEED'])
+tf.random.set_seed(DP['SEED'])
 
 def train_ffnn(X_tr, y_tr, model_name):
     [n, d] = X_tr.shape