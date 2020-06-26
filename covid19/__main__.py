Correction in SNR computation, other minor fixes

covid19/__main__.py
@@ -1,6 +1,6 @@
 import numpy as np
 from joblib import load
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score
 
 from .ml.svm import SVM
 from .ml.random_forest import random_forest
@@ -10,6 +10,10 @@ from .data.data_loader import DataLoader
 from .data.data_analytics import DataAnalytics
 from .utilities.argparser import parse_arguments
 
+def tn(y_pred, y_true): return confusion_matrix(y_true, y_pred)[0, 0]
+def fn(y_pred, y_true): return confusion_matrix(y_true, y_pred)[1, 0]
+def specificity(y_pred, y_true): return round(tn(y_true, y_pred)/ (tn(y_true, y_pred) + fn(y_true, y_pred)), DP['ROUND'])
+
 def main():
     # Command line argument parsing
     args = parse_arguments()
@@ -18,7 +22,7 @@ def main():
     dl = DataLoader(FP['DATA'])
 
     # Load the data set
-    X, y = dl.read_data(fl=(DP['CATFL']+ DP['CONFL']), ll=DP['LL'])
+    X, y = dl.read_data(fl=(DP['CATFL']+DP['CONFL']), ll=DP['LL'])
 
     X_fs = X
     if (args.dataAnalytics):
@@ -28,8 +32,8 @@ def main():
         print(f'Number of data points in class 1: {da.p_y()}')
         print('\n---------------**********---------------\n')
 
-        print(f'Auto Correlation Matrix: {da.correlation_matrix()}')
-        print('\n---------------**********---------------\n')
+        # print(f'Auto Correlation Matrix: {da.correlation_matrix()}')
+        # print('\n---------------**********---------------\n')
 
 
         print(f'SNR for numerical variables:\n')
@@ -39,8 +43,8 @@ def main():
         X_fs, fs = da.select_features(DP['CATFL'], DP['CONFL'],\
                                         k_cat=DP['NUM_CAT'],\
                                         k_con=DP['NUM_CON'],\
-                                        cat_mode='chi2',\
-                                        con_mode='anova_f')
+                                        cat_mode=DP['CAT_FS_MODE'],\
+                                        con_mode=DP['CON_FS_MODE'])
         print(f'Selected Categorical Features:\n')
         print(np.array(DP['CATFL'])[fs[0]])
 
@@ -53,27 +57,30 @@ def main():
 
     # Stratified Splitting of the dataset
     X_tr, X_t, y_tr, y_t = dl.stratified_split(X_fs, y, DP['SR'], DP['SEED'])
+    print(X_tr.shape, X_t.shape)
 
     if (args.trainSVM):
         SVM(X_tr, y_tr, 'rbf', FP['SVM_RBF'])
         SVM(X_tr, y_tr, 'linear', FP['SVM_LINEAR'])
 
-        # Load Saved SVM models and evaluate model
-        rbf_svm_mdl = load(FP['SVM_RBF'])
-        y_p = rbf_svm_mdl.predict(X_t)
-        print(f'RBF Kernel SVM Accuracy: {accuracy_score(y_p, y_t)}')
-        print(f'RBF Kernel SVM Precision: {precision_score(y_p, y_t)}')
-        print(f'RBF Kernel SVM Recall: {recall_score(y_p, y_t)}')
-        print(f'RBF Kernel SVM F1: {f1_score(y_p, y_t)}')
-        print('\n---------------**********---------------\n')
+    # Load Saved SVM models and evaluate model
+    rbf_svm_mdl = load(FP['SVM_RBF'])
+    y_p = rbf_svm_mdl.predict(X_t)
+    print(f'RBF Kernel SVM Accuracy: {accuracy_score(y_p, y_t)}')
+    print(f'RBF Kernel SVM Precision: {round(precision_score(y_p, y_t),3)}')
+    print(f'RBF Kernel SVM Sensitivity/Recall: {round(recall_score(y_p, y_t),3)}')
+    print(f'RBF Kernel SVM Specificity/Selectivity: {specificity(y_p, y_t)}')
+    print(f'RBF Kernel SVM F1: {round(f1_score(y_p, y_t),3)}')
+    print('\n---------------**********---------------\n')
 
-        linear_svm_mdl = load(FP['SVM_LINEAR'])
-        y_p = linear_svm_mdl.predict(X_t)
-        print(f'Linear Kernel SVM Accuracy: {accuracy_score(y_p, y_t)}')
-        print(f'Linear Kernel SVM Precision: {precision_score(y_p, y_t)}')
-        print(f'Linear Kernel SVM Recall: {recall_score(y_p, y_t)}')
-        print(f'Linear Kernel SVM F1 Score: {f1_score(y_p, y_t)}')
-        print('\n---------------**********---------------\n')
+    linear_svm_mdl = load(FP['SVM_LINEAR'])
+    y_p = linear_svm_mdl.predict(X_t)
+    print(f'Linear Kernel SVM Accuracy: {accuracy_score(y_p, y_t)}')
+    print(f'Linear Kernel SVM Precision: {round(precision_score(y_p, y_t),3)}')
+    print(f'Linear Kernel SVM Sensitivity/Recall: {round(recall_score(y_p, y_t),3)}')
+    print(f'Linear Kernel SVM Specificity/Selectivity: {specificity(y_p, y_t)}')
+    print(f'Linear Kernel SVM F1 Score: {round(f1_score(y_p, y_t),3)}')
+    print('\n---------------**********---------------\n')
     
     if (args.trainRF):
         random_forest(X_tr, y_tr, FP['RFMDL'])
@@ -82,9 +89,10 @@ def main():
         rfc = load(FP['RFMDL'])
         y_p = rfc.predict(X_t)
         print(f'RFC Accuracy: {accuracy_score(y_p, y_t)}')
-        print(f'RFC Precision: {precision_score(y_p, y_t)}')
-        print(f'RFC Recall: {recall_score(y_p, y_t)}')
-        print(f'RFC F1: {f1_score(y_p, y_t)}')
+        print(f'RFC Precision: {round(precision_score(y_p, y_t),3)}')
+        print(f'RFC Sensitivity/Recall: {round(recall_score(y_p, y_t),3)}')
+        print(f'RFC Specificity/Selectivity: {specificity(y_p, y_t)}')
+        print(f'RFC F1: {round(f1_score(y_p, y_t),3)}')
         print('\n---------------**********---------------\n')
 
     if (args.trainNN):